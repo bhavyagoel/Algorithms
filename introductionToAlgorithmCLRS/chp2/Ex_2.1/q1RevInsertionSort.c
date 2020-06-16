8b7957e492936b9703e167a46f992b8d157ef9eb
bhavyagoel <bgoel4132@gmail.com>
2020-06-16 15:18:18 +0530

    Beginning

---
+++ introductionToAlgorithmCLRS/chp2/Ex_2.1/q1RevInsertionSort.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int  *insertionSort(int array[], int size) {
+
+    for (int i = 1; i < size; i++) {
+        int key = array[i];
+        int j = i - 1;
+        while (j > -1 && array[j] < key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
+    return array;
+}
+int main() {
+    int size;
+    printf("Enter the size of array you need : ");
+    scanf("%d", &size);
+    int array[size];
+    printf("Enter the elements of the array \n");
+    for (int i = 0 ; i< size; i++) {
+        scanf("%d", &array[i]);
+    }
+    int *sortedArray;
+    sortedArray = insertionSort(array, size);
+
+    printf("Sorted array is : [ ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", sortedArray[i]);
+    }
+    printf("]");
+}
---
+++ introductionToAlgorithmCLRS/chp2/Ex_2.1/q1RevInsertionSort.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class q1RevInsertionSort {
+
+    public static int[] sorting (int[] array) {
+        int sortedArray[] = array;
+        for(int j = 1; j < sortedArray.length; j++) {
+            int key = sortedArray[j];
+            int i = j - 1 ;
+            while (i >-1 && sortedArray[i] < key ) {
+                sortedArray[i + 1] = sortedArray[i];
+                i = i - 1;
+            }
+            sortedArray[i+1] = key;
+        }
+        
+        return(sortedArray);
+    }
+
+    public static void main(String[] args){
+
+        int size;
+        System.out.println("Enter the size of array you wish to have :-");
+        Scanner element = new Scanner(System.in);
+        size = element.nextInt();
+        int array[] = new int[size];
+
+        System.out.println("First Defination of array is : " + Arrays.toString(array));
+        System.out.println("Enter the elements of the array");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = element.nextInt();
+        }
+
+        element.close();
+        System.out.println("Defination of array after inserting elements : " + Arrays.toString(array));
+
+        int sortedArray[] = sorting(array);
+
+        System.out.println("New Sorted array is : " + Arrays.toString(sortedArray));
+
+    }
+
+}
