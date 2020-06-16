d8e3302e1e0e7e3436c78e55f78f5cdb6f2ffcd0
bhavyagoel <bgoel4132@gmail.com>
2020-06-16 15:07:11 +0530

    Beginning

---
+++ introductionToAlgorithmCLRS/chp2/insertionSort.py
@@ -0,0 +1,20 @@
+def insertionSort(array = []):
+    sortedArray = array
+    for i in range(len(array)):
+        key = sortedArray[i]
+        j = i - 1
+        while (j > -1 and sortedArray[j] > key):
+            sortedArray[j + 1] = sortedArray[j]
+            j = j - 1
+        sortedArray[j + 1] = key
+    return sortedArray
+
+print("Enter the size of Array : ", end="")
+size = int(input())
+
+print("Enter the elements of the array : ", end="")
+array = list(map(lambda x: int(x), input().split()))
+
+sortedArray = insertionSort(array)
+
+print("New Sorted Array is : ", sortedArray)
