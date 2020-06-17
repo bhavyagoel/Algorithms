46501f2871d8c95102aaaf1dc25e9d05ea4cacae
bhavyagoel <bgoel4132@gmail.com>
2020-06-17 23:01:39 +0530

    master

--- introductionToAlgorithmCLRS/chp2/Ex_2.3/q7.java
+++ introductionToAlgorithmCLRS/chp2/Ex_2.3/q7.java
@@ -1,31 +1,64 @@
 import java.util.Scanner;
 
 public class q7 {
-    public static int binarySearchSum(int array[], int p, int r, int value) {
+
+    public static int binarySearch(int array[], int p, int r, int value) {
 
         if (r >= p){
 
             int q = (p + r) / 2;
-            int index;
             if (array[q] < value) {
-                return binarySearchSum(array, q + 1, r, value);
+                return binarySearch(array, q + 1, r, value);
             }
             else if (array[q] > value) {
-                return binarySearchSum(array, p, q, value);
+                return binarySearch(array, p, q - 1, value);
             }
             else {
-                index = q;
-                return index;
+                return q;
             }
         }
         return -1;
     }
 
+    public static int [] binarySearchSum(int array[], int size, int value) {
+        int flag = 0;
+        int retIndex[] = {-1, -1, 0};
+        for (int i = 0 ; i < size ; i++) {
+            if (value > array[i]) {
+                int subValue = value - array[i];
+                int otherIndex = binarySearch(array, 0, size - 1, subValue);
+                if (otherIndex != -1) {
+                    retIndex[0] = i;
+                    retIndex[1] = otherIndex;
+                    flag = 1;
+                    retIndex[2] = flag;
+                }
+            }
+        }
+        return retIndex;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Enter the size of array you want : ");
-        Scanner element = 
-        int index = binarySearchSum(array, 0, 9, 8);
-        System.out.println(index);
+        System.out.print("Enter the size of array you want : ");
+        Scanner element = new Scanner(System.in);
+        int size = element.nextInt();
+        int array[] = new int[size];
+        System.out.print("Enter the elements of the array : ");
+        for (int i = 0 ; i < size ; i++ ) {
+            array[i] = element.nextInt();
+        }
+
+        System.out.print("Enter the sum value : ");
+        int value = element.nextInt();
+        element.close();
+        int index[] = binarySearchSum(array, size, value);
+
+        if (index[2] != 0) {
+            System.out.println("Elements are found that sum " +value + " and the elements are [" +array[index[0]] +", " + array[index[1]]+ "]");
+        }
+        else {
+            System.out.println("Summed value is not found !!!");
+        }
 
     }
 }
