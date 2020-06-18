a62e1c20f0c1397f3bb7148dc45b8ab5ab378adc
bhavyagoel <bgoel4132@gmail.com>
2020-06-19 00:54:53 +0530

    master

--- introductionToAlgorithmCLRS/chp2/Chapter_Problems/q2BubbleSort.java
+++ introductionToAlgorithmCLRS/chp2/Chapter_Problems/q2BubbleSort.java
@@ -5,7 +5,7 @@ public class q2BubbleSort {
     public static int[] BubbleSort(int array[], int size) {
 
         for (int i = 0 ; i < size-1; i++) {
-            for (int j = size-1; j < i; j--) {
+            for (int j = size-1; j > i; j--) {
                 if (array[j] < array[j-1]) {
                     int temp = array[j];
                     array[j] = array[j-1];
@@ -15,6 +15,7 @@ public class q2BubbleSort {
         }
         return array;
     }
+
     public static void main(String[] args){
 
         int size;
@@ -34,6 +35,7 @@ public class q2BubbleSort {
         int sortedArray[] = BubbleSort(array, size);
 
         System.out.println("New Sorted array is : " + Arrays.toString(sortedArray));
+
 
     }
 }
