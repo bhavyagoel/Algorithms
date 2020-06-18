d683251cf4a45735767f7320291f1777d08c53ca
bhavyagoel <bgoel4132@gmail.com>
2020-06-19 01:26:52 +0530

    master

--- introductionToAlgorithmCLRS/chp2/Chapter_Problems/q4Inversions.java
+++ introductionToAlgorithmCLRS/chp2/Chapter_Problems/q4Inversions.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
+
 public class q4Inversions {
+    public static int Inversion(int array[], int size) {
+
+        int count = 0;
+        for(int i = 0; i < size ; i++) {
+            for (int j = i+1 ; j <size ; j++) {
+                if (array[i]>array[j]) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
     
+
+
+    public static void main(String[] args){
+
+        int size;
+        System.out.print("Enter the size of array you wish to have :- ");
+        Scanner element = new Scanner(System.in);
+        size = element.nextInt();
+        int array[] = new int[size];
+
+        System.out.print("Enter the elements of the array : ");
+
+        for (int i = 0; i < size; i++) {
+            array[i] = element.nextInt();
+        }
+
+        element.close();
+
+        int count = Inversion(array, size);
+
+        System.out.println("Count of inversion(s) is(are) : " +count);
+
+
+    }
 }
