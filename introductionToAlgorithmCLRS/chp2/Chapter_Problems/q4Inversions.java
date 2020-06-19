0aa74f61d0100c3d1cbbbaec4d72d61e2242871f
bhavyagoel <bgoel4132@gmail.com>
2020-06-19 14:36:58 +0530

    master

--- introductionToAlgorithmCLRS/chp2/Chapter_Problems/q4Inversions.java
+++ introductionToAlgorithmCLRS/chp2/Chapter_Problems/q4Inversions.java
@@ -4,7 +4,7 @@ public class q4Inversions {
     public static int Inversion(int array[], int size) {
 
         int count = 0;
-        for(int i = 0; i < size ; i++) {
+        for(int i = 0; i < size-1 ; i++) {
             for (int j = i+1 ; j <size ; j++) {
                 if (array[i]>array[j]) {
                     count++;
@@ -15,12 +15,13 @@ public class q4Inversions {
     }
 
 
-    public static int Merge(int array[], int p, int q, int r, int count) {
+    public static int Merge(int array[], int p, int q, int r) {
         int n1 = q - p + 1;
         int n2 = r - q;
+        int count = 0;
 
-        int left[] = new int[n1 + 1];
-        int right[] = new int[n2 + 1];
+        int left[] = new int[n1];
+        int right[] = new int[n2];
 
         for (int i = 0 ; i < n1; ++i) {
             left[i] = array[p + i];
@@ -29,21 +30,40 @@ public class q4Inversions {
             right[j] = array[q + j + 1];
         }
 
-        left[n1] = 99999; //Representing infinity
-        right[n2] = 99999; //Representing infinity
         int i = 0;
         int j = 0;
+        int k = p;
 
-        for (int k = p; k < r+1 ; k++) {
-            if (left[i] < right[j]) {
-                count+=1;
-                i+=1;
+        while(i<n1 && j <n2) {
+            if (left[i] <= right[j]) {
+                array[k] = left[i];
+                i = i + 1;
             }
+            else {
+                array[k] = right[j];
+                count ++;
+                j = j + 1;
+            }
+            k++;
+        }
 
+        while(i < n1) {
+            array[k] = left[i];
+            count ++;
+            i++;
+            k++;
+        }
+
+        while(j < n2) {
+            array[k] = right[j];
+            //count ++;
+            k++;
+            j++;
         }
     
         return count;
     }
+
 
     public static int MergeInversion(int array[], int p, int r) {
         int count = 0;
@@ -51,7 +71,7 @@ public class q4Inversions {
             int q = (p+r)/2;
             count += MergeInversion(array, p, q);
             count += MergeInversion(array, q+1, r);
-            count += Merge(array, p, q, r, count);
+            count += Merge(array, p, q, r);
         }
         return count;
     }
@@ -74,6 +94,8 @@ public class q4Inversions {
         int count = Inversion(array, size);
         int count2 = MergeInversion(array, 0, size-1);
         System.out.println("Count of inversion(s) is(are) : " +count2);
+        System.out.println("Count of inversion(s) is(are) : " +count);
+
 
 
     }
