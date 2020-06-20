462631fd48a0565148c54fe9af355d877ee1306b
bhavyagoel <bgoel4132@gmail.com>
2020-06-20 22:39:18 +0530

    master

--- introductionToAlgorithmCLRS/chp4/InChaptAlgo/MatrixMutliplication.java
+++ introductionToAlgorithmCLRS/chp4/InChaptAlgo/MatrixMutliplication.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MatrixMutliplication {
     
-    public static int[][] ArrayMulti(int arrayA[], int arrayB[], int size) {
+    public static int[][] ArrayMulti(int arrayA[][], int arrayB[][], int size) {
         int result[][] = new int[size][size];
         for (int i = 0 ; i < size ; i ++) {
             for (int j = 0 ; j < size ;j++) {
@@ -20,6 +20,36 @@ public class MatrixMutliplication {
     }
 
     public static void main(String[] args) {
+        Scanner element = new Scanner(System.in);
+        System.out.print("Enter the size of array : ");
+        int size = element.nextInt();
 
+        int arrayA[][] = new int[size][size];
+        int arrayB[][] = new int[size][size];
+        System.out.println("Enter the elements for array A");
+        for (int i = 0 ; i < size ; i++) {
+            System.out.print("Enter element of row (" +(i+1)+") : ");
+            for(int j = 0 ; j < size ; j++) {
+                arrayA[i][j] = element.nextInt();
+            }
+        }
+
+        System.out.println("Enter the elements for array B");
+        for (int i = 0 ; i < size ; i++) {
+            System.out.print("Enter element of row (" +(i+1)+") : ");
+            for(int j = 0 ; j < size ; j++) {
+                arrayB[i][j] = element.nextInt();
+            }
+        }    
+        element.close();
+        System.out.println("Array A is : ");
+        for (int i = 0 ; i < size ; i++) {
+            System.out.println("\t\t" +Arrays.toString(arrayA[i]));
+        }
+        
+        System.out.println("Array B is : ");
+        for (int i = 0 ; i < size ; i++) {
+            System.out.println("\t\t" +Arrays.toString(arrayB[i]));
+        }
     }
 }
