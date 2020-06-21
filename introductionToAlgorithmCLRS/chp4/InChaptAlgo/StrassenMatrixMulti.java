ae551ccd3f8258c725ec5f809248bff941e8686a
bhavyagoel <bgoel4132@gmail.com>
2020-06-21 16:59:01 +0530

    master

--- introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.java
+++ introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.java
@@ -35,6 +35,14 @@ public class StrassenMatrixMulti {
 
         return C;
     }
+
+    public void Join(int[][] C, int[][] P, int iB, int jB) {
+        for (int i1 = 0, i2 = iB; i1 < C.length ; i1++, i2++) {
+            for(int j1 =0, j2 = jB ; j1 < C.length ;j1++, j2++) {
+                P[i2][j2] = C[i1][j1];
+            }
+        }
+    }
     
     public int[][] Multiply(int[][] arrayA, int [][] arrayB) {
         int n = arrayA.length;
@@ -64,6 +72,20 @@ public class StrassenMatrixMulti {
             split(arrayB, B12, 0, n/2);
             split(arrayB, B21, n/2, 0);
             split(arrayB, B22, n/2, n/2);
+
+            int[][] M1 = Multiply(Add(A11, A22), Add(B11, B22));
+            int[][] M2 = Multiply(Add(A21, A22), B11);
+            int[][] M3 = Multiply(A11, Sub(B12, B22));
+            int[][] M4 = Multiply(A22, Sub(B21, B11));
+            int[][] M5 = Multiply(Add(A11, A12), B22);
+            int[][] M6 = Multiply(Sub(A21, A11), Add(B11, B12));
+            int[][] M7 = Multiply(Sub(A12, A22), Add(B21, B22));
+
+            int[][] C11 = Add(Sub(Add(M1, M4), M5), M7);
+            int[][] C12 = Add(M3, M5);
+            int[][] C21 = Add(M2, M4);
+            int[][] C22 = Add(Sub(Add(M1, M3), M2), M6);
+
 
             
 
