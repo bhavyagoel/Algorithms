29a341213f7f2fd5c4efd64f547d020040e32079
bhavyagoel <bgoel4132@gmail.com>
2020-06-21 14:21:38 +0530

    master

--- introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.java
+++ introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+import java.util.Arrays; 
+
+
 public class StrassenMatrixMulti {
     
+    public static int[][] MatrixMultiply(int arrayA[][], int arrayB[][], int rowA, int colA, int rowB, int colB) {
+        int arrayC[][] = new int[rowA][colB];
+
+        for (int i = 0 ; i < rowA ; i++) {
+            for(int j = 0 ; j < colB ; j++) {
+                arrayC[i][j] = 0;
+                for (int k = 0 ; k < colA ; k++) {
+                    arrayC[i][j] += arrayA[i][k] * arrayB[k][j];
+                }
+            }
+        }
+
+        return arrayC;
+
+    }
+
+    public static int[][] Strassen(int arrayA[][], int arrayB[][], int rowA, int colA, int rowB, int colB) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
