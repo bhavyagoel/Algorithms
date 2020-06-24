ee4233af3402b63fae4877e66d9bba191eb60915
bhavyagoel <bgoel4132@gmail.com>
2020-06-24 15:11:35 +0530

    master

--- introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.c
+++ introductionToAlgorithmCLRS/chp4/InChaptAlgo/StrassenMatrixMulti.c
@@ -147,3 +147,81 @@ int **Multiply(int **arrayA, int **arrayB) {
 
     return R;
 }
+
+
+void main() {
+
+    printf("Enter the size of array : ");
+    int size;
+    scanf("%d", &size);
+
+    while (((int)(ceil((log(size) / log(2)))) != (int)(floor((log(size) / log(2)))))) {
+        printf("Enter size to be a power of 2 : ");
+        scanf("%d", &size);
+    }
+
+    int **arrayA;
+    arrayA = malloc(size * sizeof(*arrayA));
+    for(int i = 0 ; i < size ; i++) {
+        arrayA[i] = malloc(size * sizeof(*arrayA));
+    }
+    printf("Enter the elements for array A \n");
+    for (int i = 0 ; i < size ; i++) {
+        printf("Enter element of row (%d) : " ,(i+1));
+        for(int j = 0 ; j < size ; j++) {
+            scanf("%d", &arrayA[i][j]);
+        }
+    }
+    int **arrayB;
+    arrayB = malloc(size * sizeof(*arrayB));
+    for(int i = 0 ; i < size ; i++) {
+        arrayB[i] = malloc(size * sizeof(*arrayB));
+    }
+
+    printf("Enter the elements for array B \n");
+    for (int i = 0 ; i < size ; i++) {
+        printf("Enter element of row (%d) : " ,(i+1));
+        for(int j = 0 ; j < size ; j++) {
+            scanf("%d", &arrayB[i][j]);
+        }
+    }
+
+
+    printf("Array A is : \n");
+    for (int i = 0 ; i < size ; i++) {
+        printf("\t\t[ ");
+        for(int j = 0 ; j < size ; j++) {
+            printf("%d ",arrayA[i][j]);
+        }
+        printf("]\n");
+    }
+
+
+    printf("Array B is : \n");
+    for (int i = 0 ; i < size ; i++) {
+        printf("\t\t[ ");
+        for(int j = 0 ; j < size ; j++) {
+            printf("%d ",arrayB[i][j]);
+        }
+        printf("]\n");
+    }
+    int n = sizeof(arrayA);
+    int p = sizeof(**arrayA);
+    printf("%d %d \n", n, p);
+    // int **arrayC;
+    // arrayC = malloc((size+1) * sizeof(*arrayC));
+    // for(int i = 0 ; i < size+1 ; i++) {
+    //     arrayC[i] = malloc((size+1) * sizeof(*arrayC));
+    // }
+    //int **arrayC = Multiply(arrayA, arrayB);
+    // printf("Array C is : \n");
+    // for (int i = 0 ; i < size ; i++) {
+    //     printf("\t\t[ ");
+    //     for(int j = 0 ; j < size ; j++) {
+    //         printf("%d ",arrayC[i][j]);
+    //     }
+    //     printf("]\n");
+    // }
+}
+
+
