03238e88003346f99bb8ccc43b824a09d110874a
bhavyagoel <bgoel4132@gmail.com>
2020-06-25 12:44:54 +0530

    master

--- introductionToAlgorithmCLRS/chp6/Exercises/Ex_6.2/q5LoopMaxHeapify.java
+++ introductionToAlgorithmCLRS/chp6/Exercises/Ex_6.2/q5LoopMaxHeapify.java
@@ -3,66 +3,41 @@ import java.util.Arrays;
 public class q5LoopMaxHeapify {
     
     public static int Parent(int i) {
-        return ((i-1)/2);
+        return ((i)/2);
     }
 
     public static int Left(int i) {
-        return ((2*i)+1);
+        return ((2*i));
     }
 
     public static int Right(int i) {
-        return ((2*i)+2);
+        return ((2*i)+1);
     }
 
     public static void Max(int array[], int i, int size) {
-        int largest = -1;
-        while (i < size) {
+        do {
             int l = Left(i);
             int r = Right(i);
+            int largest = i;
             if (l<size && array[l]>array[i]) {
                 largest = l;
             }
-            else {
-                largest = i;
-            }
-            if (r < size && array[r]>array[i]) {
+
+            if (l < size && array[r]>array[i]) {
                 largest = r;
             }
+
             if (largest != i) {
                 int temp = array[i];
                 array[i] = array[largest];
                 array[largest] = temp;
                 i = largest;
             }
-            
-            // else {
-            //     largest = i;
-            // }
-            // if (largest != i) {
-            //     int temp = array[i];
-            //     array[i] = array[largest];
-            //     array[largest] = temp;
-            //     i = largest;
-            // }
             else {
                 break;
             }
-        }
-        
-               
-            
-
-            
-        //     else {
-        //         largest = i;
-        //     }
-            
-               
-        //     temp = array[i];
-        //     array[i] = array[largest];
-        //     array[largest] = temp;
-
-        
+ 
+        } while (i < size);
 
     }
 
--- introductionToAlgorithmCLRS/chp6/InChaptAlgo/MaxHeapify.java
+++ introductionToAlgorithmCLRS/chp6/InChaptAlgo/MaxHeapify.java
@@ -32,7 +32,7 @@ public class MaxHeapify {
             array[largest] = temp;
             Max(array, largest, size);
         }
-        if (r < size && array[r]>array[i]) {
+        if (l < size && array[r]>array[i]) {
             largest = r;
         }
         else {
